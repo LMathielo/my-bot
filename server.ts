c7f4a0cd21ff9de2c122481cddbca890bc3c1ae1
guilherme <joao.g.f@icloud.com>
2022-10-05 17:36:33 -0300

    first commit

---
+++ server.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import { Client, LocalAuth, MessageMedia } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
+import axios from 'axios';
+import cors from 'cors'
+
+export const app = express();
+
+app.use(cors());
+
+const client = new Client({
+  puppeteer: {
+    headless: true,
+  },
+  authStrategy: new LocalAuth(),
+});
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+client.on('message_create', async msg => {
+  if (msg.body === '!cat') {
+    const { data } = await axios('https://aws.random.cat/meow');
+
+    const media = await MessageMedia.fromUrl(data.file);
+
+    media.mimetype = 'image/png';
+    media.filename = 'CustomImageName.png';
+    msg.reply(media, undefined, { sendMediaAsSticker: true });
+  }
+
+  if (msg.body === '!capivara') {
+    const { data } = await axios(
+      'https://api.capy.lol/v1/capybara?json=true',
+    );
+
+    const media = await MessageMedia.fromUrl(data.data.url, {
+      unsafeMime: true,
+    });
+
+    media.mimetype = 'image/png';
+    media.filename = 'CustomImageName.png';
+    msg.reply(media, undefined, { sendMediaAsSticker: true });
+  }
+
+  if (msg.hasMedia && msg.body === '!sticker') {
+    const media = await msg.downloadMedia();
+
+    msg.reply(media, undefined, { sendMediaAsSticker: true });
+  }
+});
+
+client.initialize();
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+})
